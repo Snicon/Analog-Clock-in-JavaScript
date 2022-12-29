4cba3b317fb41b2d4dc12f61ff1bbad847d63b27
Sixten Peterson <sixten.peterson@winterzone.se>
2022-12-29 20:59:10 +0100

    Added js logic for the clock

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+
+
+// Hand vars
+const secondHand = document.querySelector('.second')
+const minuteHand = document.querySelector('.minute')
+const hourHand = document.querySelector('.hour')
+
+// Setting the clock to the right time.
+const setClock = () => {
+    const date = new Date();
+    const secondsRatio = date.getSeconds() / 60
+    // Minutes & hours should move smoothly and not suddenly change position. -> Hence the "weird" math
+    const minutesRatio = (secondsRatio + date.getMinutes()) / 60
+    const hoursRatio = (secondsRatio + minutesRatio + date.getHours()) / 12
+
+    // Rotate the hands to the right positions
+    setRotation(secondHand, secondsRatio)
+    setRotation(minuteHand, minutesRatio)
+    setRotation(hourHand, hoursRatio)
+}
+
+// While setPropert is self-explanatory it's not something that has been taught in the course for Codecademy, to read more visit: https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleDeclaration/setProperty
+const setRotation = (element, rotationRatio) => element.style.setProperty('--rotation', rotationRatio * 360)
+
+// Calling the setClock function once every second (1000 milliseconds) in order to "updated the clock" and make it tick.
+// To read more about the setInterval method goto this page: https://developer.mozilla.org/en-US/docs/Web/API/setInterval
+setInterval(setClock, 1000)
